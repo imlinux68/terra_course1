73e024b4e4d76185794545ef96be6abea7992476
im linux <imlinux68@gmail.com>
2023-04-24 23:54:11 +0300

    added dynamic files

--- main.tf
+++ main.tf
@@ -52,11 +52,15 @@ resource "aws_security_group" "MyLab-sg1" {
     description = "To allow inbound and outbound traffic for my lab"
     vpc_id = aws_vpc.MyLab_VPC.id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+    dynamic ingress {
+        iterator = port
+        for_each = var.ports
+        content {
+            from_port = port.value
+            to_port = port.value
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
     }
 
     egress {
